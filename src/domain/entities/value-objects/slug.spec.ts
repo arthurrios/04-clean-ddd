823b8c4cf08f1fab2d71b7ca8ed09995ff4e35d4
arthur <arthur.rios007@gmail.com>
2025-01-05 15:24:10 -0300

    chore: create slug value object

---
+++ src/domain/entities/value-objects/slug.spec.ts
@@ -0,0 +1,8 @@
+import { expect, test } from 'vitest'
+import { Slug } from './slug'
+
+test('should be able to create a new slug from text', async () => {
+  const slug = Slug.createFromText('An example title')
+
+  expect(slug.value).toEqual('an-example-title')
+})
