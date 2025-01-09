c3bf54320c69e26fdd1014db37a577a0aaa21612
arthur <arthur.rios007@gmail.com>
2025-01-09 18:37:43 -0300

    feat: add read notification use case

--- src/domain/notification/enterprise/entities/notification.ts
+++ src/domain/notification/enterprise/entities/notification.ts
@@ -2,7 +2,7 @@ import { Entity } from '@/core/entities/entity'
 import type { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import type { Optional } from '@/core/types/optional'
 
-interface NotificationProps {
+export interface NotificationProps {
   recipientId: UniqueEntityID
   title: string
   content: string
@@ -25,6 +25,10 @@ export class Notification extends Entity<NotificationProps> {
 
   get createdAt() {
     return this.props.createdAt
+  }
+
+  read() {
+    this.props.readAt = new Date()
   }
 
   get readAt() {
